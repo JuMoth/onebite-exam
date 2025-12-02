c014500df7a0791fd40378932fee524bc5c55340
JuMoth <tkdlqjqkd@gmail.com>
2025-12-02 11:06:07 +0900

    feat: Shadcn Core UI Usage1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import { Textarea } from "./components/ui/textarea";
+import { Toaster } from "./components/ui/sonner";
 import { cn } from "./lib/utils";
+import { toast } from "sonner";
 
 function App() {
   const isActive = true;
 
   return (
-    <div>
-      <Button>버튼!</Button>
+    <div className="p-5">
+      <Toaster />
+
+      <Textarea />
+
+      <Input placeholder="입력 ..." />
+
+      <Button
+        onClick={() => {
+          toast("토스트 메세지", {
+            position: "top-center",
+          });
+        }}
+      >
+        버튼!
+      </Button>
+      <Button variant={"destructive"}>버튼</Button>
+      <Button variant={"ghost"}>버튼</Button>
+      <Button variant={"link"}>버튼</Button>
+      <Button variant={"outline"}>버튼</Button>
+      <Button variant={"secondary"}>버튼</Button>
 
       <div
         className={cn(
