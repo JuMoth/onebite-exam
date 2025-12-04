22c9cd2822382b336f2e8d63374ea46360a2172e
JuMoth <tkdlqjqkd@gmail.com>
2025-12-04 19:42:04 +0900

    feat: Zustand Usage 2

--- src/store/count.ts
+++ src/store/count.ts
@@ -2,22 +2,41 @@ import { create } from "zustand";
 
 type Store = {
     count: number;
-    increase: () => void;
-    decrease: () => void;
+    actions: {
+        increase: () => void;
+        decrease: () => void;
+    };
 };
 
 export const useCountStore = create<Store>((set, get) => ({
     count: 0,
-    increase: () => {
-        const count = get().count;
+    actions: {
+        increase: () => {
+            const count = get().count;
 
-        set((store) => ({
-            count: store.count + 1,
-        }));
-    },
-    decrease: () => {
-        set((store) => ({
-            count: store.count - 1,
-        }));
+            set((store) => ({
+                count: store.count + 1,
+            }));
+        },
+        decrease: () => {
+            set((store) => ({
+                count: store.count - 1,
+            }));
+        },
     },
 }));
+
+export const useCount = () => {
+    const count = useCountStore((store) => store.count);
+    return count;
+};
+
+export const useIncreaseCount = () => {
+    const increase = useCountStore((store) => store.actions.increase);
+    return increase;
+};
+
+export const useDecreaseCount = () => {
+    const decrease = useCountStore((store) => store.actions.decrease);
+    return decrease;
+};
