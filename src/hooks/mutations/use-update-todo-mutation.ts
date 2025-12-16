d49c9710a97631a2fca6c551c39622b77cca56f8
JuMoth <tkdlqjqkd@gmail.com>
2025-12-16 23:16:46 +0900

    feat: Optimistic Update2

--- src/hooks/mutations/use-update-todo-mutation.ts
+++ src/hooks/mutations/use-update-todo-mutation.ts
@@ -8,7 +8,12 @@ export function useUpdateTodoMutation() {
 
     return useMutation({
         mutationFn: updateTodo,
-        onMutate: (updatedTodo) => {
+        onMutate: async (updatedTodo) => {
+            await queryClient.cancelQueries({
+                queryKey: QUERY_KEYS.todo.list,
+            });
+
+            const prevTodos = queryClient.getQueryData<Todo[]>(QUERY_KEYS.todo.list);
             queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
                 if (!prevTodos) return [];
                 return prevTodos.map(prevTodo =>
@@ -17,6 +22,18 @@ export function useUpdateTodoMutation() {
                         : prevTodo,
                 );
             });
+
+            return {
+                prevTodos,
+            };
         },
+        onError: (error, variable, context) => {
+            if (context && context.prevTodos) {
+                queryClient.setQueryData<Todo[]>(
+                    QUERY_KEYS.todo.list,
+                    context.prevTodos,
+                );
+            }
+        }
     });
 }
